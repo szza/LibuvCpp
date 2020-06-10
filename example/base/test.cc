534a5e6b7d5bf9aaa7943363bdce95f7365dceeb
szza <szz13883079847@163.com>
2020-06-10 21:41:57 +0800

    usage

--- example/base/test.cc
+++ example/base/test.cc
@@ -3,24 +3,17 @@
 #include <string.h>
 
 #include <string>
+#include <memory>
 #include <unordered_map>
 
 int main(int argc, char const *argv[]) {
   std::cout<<std::boolalpha;
   
-  std::unordered_map<const char*, int> map_;
-
-  const char* src1 = "some";
-  const char* src2 = "some";
-  std::string src3("some");
-
-  map_.emplace(src1, 1);
-
-  auto iter2 = map_.find(src2);
-  auto iter3 = map_.find(src3.c_str());
-
-
-  std::cout<<"can find:"<<src1<<" ? "<<(iter2 != map_.end())<<std::endl;
-  std::cout<<"can find:"<<src1<<" ? "<<(iter3 != map_.end())<<std::endl;
-
+  std::shared_ptr<int> intPtr =std::make_shared<int>(10);
+  {
+    auto iptr =  intPtr;
+    iptr.reset();
+  }
+  std::cout<<*intPtr<<std::endl;
+  return 0;
 }
